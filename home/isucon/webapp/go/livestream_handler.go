d1dbce51d3d88b5655760f4ea9244b9ff23b2a31
“inakuuun” <bkg.inamoto@gmail.com>
2024-12-01 14:30:14 +0900

    不要なトランザクションを全体的に削除②

--- home/isucon/webapp/go/livestream_handler.go
+++ home/isucon/webapp/go/livestream_handler.go
@@ -262,10 +262,6 @@ func getMyLivestreamsHandler(c echo.Context) error {
 		livestreams[i] = livestream
 	}
 
-	if err := tx.Commit(); err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "failed to commit: "+err.Error())
-	}
-
 	return c.JSON(http.StatusOK, livestreams)
 }
 
@@ -413,10 +409,6 @@ func getLivestreamHandler(c echo.Context) error {
 	livestream, err := fillLivestreamResponse(ctx, tx, livestreamModel)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to fill livestream: "+err.Error())
-	}
-
-	if err := tx.Commit(); err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "failed to commit: "+err.Error())
 	}
 
 	return c.JSON(http.StatusOK, livestream)
